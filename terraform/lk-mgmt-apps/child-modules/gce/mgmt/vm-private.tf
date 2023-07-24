e5d781d6260b52014ab04c235a07088af1d6a119
Tinkal Shakya <111516273+tinkalshakya-cloudside@users.noreply.github.com>
2023-07-24 19:46:23 +0530

    Update vm-private.tf

--- terraform/lk-mgmt-apps/child-modules/gce/mgmt/vm-private.tf
+++ terraform/lk-mgmt-apps/child-modules/gce/mgmt/vm-private.tf
@@ -1,5 +1,5 @@
 module "vm-public" {
-    source              = "../../modules/vm-private"
+    source              = "../../../modules/gce"
     zone                = var.zone
     labels              = var.labels
     image               = var.image
