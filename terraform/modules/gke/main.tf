924a871feeb4a083c711f500bbc8f94c27d08a3d
Tinkal Shakya <111516273+tinkalshakya-cloudside@users.noreply.github.com>
2023-07-25 16:30:37 +0530

    Update main.tf

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -1,4 +1,4 @@
-resource "google_container_cluster" "gke-test-cluster-01" {
+resource "google_container_cluster" "gke-cluster-01" {
   enable_shielded_nodes       = var.enable_shielded_nodes
   remove_default_node_pool    = var.remove_default_node_pool
   initial_node_count          = var.initial_node_count
@@ -44,12 +44,47 @@ resource "google_container_cluster" "gke-test-cluster-01" {
     workload_pool = var.workload_pool
   }
 }
-resource "google_container_node_pool" "node-pool-e2-m" {
+resource "google_container_node_pool" "node-pool-asso2-mgmt-01" {
  autoscaling {
   max_node_count= var.max_node_count
   min_node_count= var.min_node_count
  }
   cluster            = google_container_cluster.gke-test-cluster-01.name
+  location           = var.location
+  project            = var.project
+  management {8
+    auto_repair  = var.auto_repair
+    auto_upgrade = var.auto_upgrade
+  }
+  max_pods_per_node = var.max_pods_per_node
+  name              = var.nodepool_name
+  node_config {
+    disk_size_gb    = var.disk_size_gb
+    disk_type       = var.disk_type
+    image_type      = var.image_type
+    local_ssd_count = var.local_ssd_count
+    machine_type    = var.machine_type
+    oauth_scopes    = var. oauth_scopes
+    service_account = var. service_account
+    shielded_instance_config {
+      enable_integrity_monitoring = var.enable_integrity_monitoring
+      enable_secure_boot          = var.enable_secure_boot
+    }
+  }
+  node_count     = var.node_count
+  upgrade_settings {
+    max_surge       = var.max_surge
+    max_unavailable = var.max_unavailable
+  }
+  version = var.node_version
+}
+
+resource "google_container_node_pool" "node-pool-asso2-mgmt-02" {
+ autoscaling {
+  max_node_count= var.max_node_count
+  min_node_count= var.min_node_count
+ }
+  cluster            = google_container_cluster.gke-cluster-01.name
   location           = var.location
   project            = var.project
   management {
